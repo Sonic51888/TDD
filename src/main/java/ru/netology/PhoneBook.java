1168bcda5fb9308cf14452897508763b136826ae
Baliuk Alexander <Sonic51888@mail.ru>
2024-02-14 10:36:41 +0300

    Stub and tests for add

--- src/main/java/ru/netology/PhoneBook.java
+++ src/main/java/ru/netology/PhoneBook.java
@@ -1,4 +1,52 @@
 package ru.netology;
 
+import java.util.Objects;
+
 public class PhoneBook {
+   private String name ;
+    private long phoneNumber;
+
+
+
+    public int add(String name, long phoneNumber) {
+        return 0;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        PhoneBook phoneBook = (PhoneBook) o;
+        return phoneNumber == phoneBook.phoneNumber && Objects.equals(name, phoneBook.name);
+    }
+
+
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, phoneNumber);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public long getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(long phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+    @Override
+    public String toString() {
+        return "PhoneBook{" +
+                "name='" + name + '\'' +
+                ", phoneNumber=" + phoneNumber +
+                '}';
+    }
 }
--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -1,2 +1,15 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import ru.netology.PhoneBook;
+
 public class PhoneBookTest {
+    static PhoneBook phoneBook = new PhoneBook();
+
+    @Test
+    public void addTest() {
+        Assertions.assertEquals(1, phoneBook.add("Саша", 80_000_000_000L));
+        Assertions.assertEquals(2, phoneBook.add("Аня", 81_111_111_111L));
+        Assertions.assertEquals(3, phoneBook.add("Тоня", 82_222_222_222L));
+    }
+
 }
